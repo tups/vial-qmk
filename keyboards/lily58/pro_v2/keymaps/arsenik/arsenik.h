db177ff0b387e37a90688611a3795f1459e8f20d
Damien TUPINIER <tupinier.damien@gmail.com>
2026-06-06 19:57:03 +0200

    Add OS-aware shortcuts and improve Arsenik keymap for Lily58 Pro V2

--- keyboards/lily58/pro_v2/keymaps/arsenik/arsenik.h
+++ keyboards/lily58/pro_v2/keymaps/arsenik/arsenik.h
@@ -309,18 +309,18 @@
 
 #elif defined ARSENIK_LAYOUT_lily58_pro_v2
 #define ARSENIK_LAYOUT(\
-        k11, k12, k13, k14, k15, k16,     k17, k18, k19, k1a, k1b, k1c,\
-        k21, k22, k23, k24, k25, k26,     k27, k28, k29, k2a, k2b, k2c,\
-        k31, k32, k33, k34, k35, k36,     k37, k38, k39, k3a, k3b, k3c,\
-        k41, k42, k43, k44, k45, k46,     k47, k48, k49, k4a, k4b, k4c,\
-              k50, k51, k52, k53,         k54, k55, k56, k57\
+        k11, k12, k13, k14, k15, k16,          k17, k18, k19, k1a, k1b, k1c,\
+        k21, k22, k23, k24, k25, k26,          k27, k28, k29, k2a, k2b, k2c,\
+        k31, k32, k33, k34, k35, k36,          k37, k38, k39, k3a, k3b, k3c,\
+        k41, k42, k43, k44, k45, k46, k4i, k4j, k47, k48, k49, k4a, k4b, k4c,\
+              k50, k51, k52, k53,              k54, k55, k56, k57\
     ) \
     LAYOUT(\
         /* Row 0 */ k11, k12, k13, k14, k15, k16,                     k17, k18, k19, k1a, k1b, k1c, \
         /* Row 1 */ k21, k22, k23, k24, k25, k26,                     k27, k28, k29, k2a, k2b, k2c, \
         /* Row 2 */ k31, k32, k33, k34, k35, k36,                     k37, k38, k39, k3a, k3b, k3c, \
         /* Row 3 left + center inserts + Row 3 right */ \
-                    k41, k42, k43, k44, k45, k46,   XX, XX,           k47, k48, k49, k4a, k4b, k4c, \
+                    k41, k42, k43, k44, k45, k46, k4i, k4j,           k47, k48, k49, k4a, k4b, k4c, \
         /* Thumbs (left 4 then right 4) */ \
                     k50, k51, k52, k53,                                   k54, k55, k56, k57 \
     )
--- keyboards/lily58/pro_v2/keymaps/arsenik/keymap.c
+++ keyboards/lily58/pro_v2/keymaps/arsenik/keymap.c
@@ -19,6 +19,29 @@ enum custom_keycodes {
     ODK_5,  // ‰
 };
 
+// Custom keycodes exposed to Vial. Must match the order of the
+// "customKeycodes" array in vial.json (index 0 -> QK_KB_0, etc.).
+enum vial_custom_keycodes {
+    VRGB_TOG = QK_KB_0,  // "RGB Toggle"
+    OS_UNDO,             // Ctrl+Z / Cmd+Z  (selon AG_TOGG)
+    OS_CUT,              // Ctrl+X / Cmd+X
+    OS_COPY,             // Ctrl+C / Cmd+C
+    OS_PASTE,            // Ctrl+V / Cmd+V
+    OS_ALL,              // Ctrl+A / Cmd+A
+    OS_SAVE,             // Ctrl+S / Cmd+S
+    OS_NTAB,             // Ctrl+T / Cmd+T
+    MAC_TOGG,            // Toggle Win/Mac : swap LAlt<->LGui uniquement.
+                         // (AG_TOGG swappe aussi RAlt<->RGui, ce qui casse
+                         // AltGr utilisé par QwertyLafayette sur RALT_T)
+};
+
+// Sends Ctrl+kc on Windows mode, Cmd+kc on Mac mode (toggled with AG_TOGG).
+// tap_code16 bypasses the magic Alt<->GUI remapping, so the modifier we
+// send here is exactly what the OS receives.
+static inline void tap_os_shortcut(uint16_t kc) {
+    tap_code16(keymap_config.swap_lalt_lgui ? G(kc) : C(kc));
+}
+
 // The ARSENIK_LAYOUT macro allows us to declare a config for a 4x6+3 keyboard, then truncate it
 // (or fill it with noops) depending on the size of your keyboard. Your keyboard may have extra
 // definitions for this macro or none at all (preventing you from compiling the keymap). Check
@@ -35,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_1, KC_2,  KC_3,  KC_4,  KC_5,      KC_6, KC_7,  KC_8,    KC_9,   KC_0,    KC_DEL,
         KC_TAB,  KC_Q, KC_W,  KC_E,  KC_R,  KC_T,      KC_Y, KC_U,  KC_I,    KC_O,   KC_P,    KC_INS,
         KC_ESC,  KC_A, KC_SS, KC_DD, KC_FF, KC_G,      KC_H, KC_JJ, KC_KK,   KC_LL,  KC_SCLN, KC_ENTER,
-        KC_LSFT, KC_Z, KC_X,  KC_C,  KC_V,  KC_B,      KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LSFT, KC_Z, KC_X,  KC_C,  KC_V,  KC_B, MAC_TOGG, XX, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
         MO(_vim_nav), LT(_fun_pad, KC_ENT), KC_BSPC, LT(_num_nav, KC_SPC),    LT(_num_nav, KC_SPC), RALT_T(KC_ENT), MO(_vim_nav), MO(_fun_pad)
     ),
 
@@ -58,17 +81,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_vim_nav] = ARSENIK_LAYOUT(
         __, G(KC_1),      G(KC_2),  G(KC_3),   G(KC_4),  G(KC_5), G(KC_6),  G(KC_7),  G(KC_8), G(KC_9), G(KC_0),      __,
-        __, MO(_num_nav), C(AS(T)), KC_WBAK,   KC_WFWD,  XX,      KC_HOME,  KC_PGDN,  KC_PGUP, KC_END,  G(AS(P)),     __,
-        __, C(AS(A)),     C(AS(S)), S(KC_TAB), KC_TAB,   XX,      KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, MO(_fun_pad), __,
-        __, C(AS(Z)),     C(AS(X)), C(AS(C)),  C(AS(V)), XX,      KC_WH_L,  KC_WH_D,  KC_WH_U, KC_WH_R, XX,           __,
+        __, MO(_num_nav), OS_NTAB,  KC_WBAK,   KC_WFWD,  XX,      KC_HOME,  KC_PGDN,  KC_PGUP, KC_END,  G(AS(P)),     __,
+        __, OS_ALL,       OS_SAVE,  S(KC_TAB), KC_TAB,   XX,      KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT, MO(_fun_pad), __,
+        __, OS_UNDO,      OS_CUT,   OS_COPY,   OS_PASTE, XX, XX, XX, KC_WH_L,  KC_WH_D,  KC_WH_U, KC_WH_R, XX,           __,
                                     __, KC_DEL,   __,   XX,      XX,   __,   KC_ESC, __
     ),
 
     [_num_nav] = ARSENIK_LAYOUT(
         __, G(KC_1),  G(KC_2),  G(KC_3),  G(KC_4),  G(KC_5),        G(KC_6),  G(KC_7),  G(KC_8), G(KC_9), G(KC_0),  __,
         __, KC_TAB,   KC_HOME,  KC_UP,    KC_END,   KC_PGUP,        AS(SLSH), AS(7),    AS(8),   AS(9),   G(AS(P)), __,
-        __, C(AS(A)), KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,        AS(MINS), AS(4),    AS(5),   AS(6),   AS(0),    __,
-        __, C(AS(Z)), C(AS(X)), C(AS(C)), C(AS(V)), S(KC_TAB),      AS(COMM), AS(1),    AS(2),   AS(3),   AS(DOT),  __,
+        __, OS_ALL,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,        AS(MINS), AS(4),    AS(5),   AS(6),   AS(0),    __,
+        __, OS_UNDO,  OS_CUT,   OS_COPY,  OS_PASTE, S(KC_TAB), XX, XX, AS(COMM), AS(1),    AS(2),   AS(3),   AS(DOT),  __,
                                     __, KC_DEL,   __,   XX,      XX,   __,   KC_ESC, __
     ),
 
@@ -76,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         __, __,    __,     __,     __,     __,      __, __,      __,      __,      __, __,
         __, KC_F1, KC_F2,  KC_F3,  KC_F4,  XX,      XX, XX,      XX,      XX,      XX, __,
         __, KC_F5, KC_F6,  KC_F7,  KC_F8,  XX,      XX, KC_LCTL, KC_LALT, KC_LGUI, __, __,
-        __, KC_F9, KC_F10, KC_F11, KC_F12, XX,      XX, XX,      XX,      XX,      XX, __,
+        __, KC_F9, KC_F10, KC_F11, KC_F12, XX, XX, XX, XX, XX,      XX,      XX,      XX, __,
                     __, __,   KC_SPC,   XX,      XX,   KC_SPC,   __, __
     ),
 
@@ -119,6 +142,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         case ODK_3: ODK3_SEQUENCE; return false;
         case ODK_4: ODK4_SEQUENCE; return false;
         case ODK_5: ODK5_SEQUENCE; return false;
+
+        case VRGB_TOG:
+#           ifdef RGB_MATRIX_ENABLE
+            rgb_matrix_toggle();
+#           endif
+            return false;
+
+        // OS-aware shortcuts: follow the AG_TOGG (Win/Mac) state.
+        case OS_UNDO:  tap_os_shortcut(AS(Z)); return false;
+        case OS_CUT:   tap_os_shortcut(AS(X)); return false;
+        case OS_COPY:  tap_os_shortcut(AS(C)); return false;
+        case OS_PASTE: tap_os_shortcut(AS(V)); return false;
+        case OS_ALL:   tap_os_shortcut(AS(A)); return false;
+        case OS_SAVE:  tap_os_shortcut(AS(S)); return false;
+        case OS_NTAB:  tap_os_shortcut(AS(T)); return false;
+
+        case MAC_TOGG:
+            // Swap LAlt<->LGui only. RAlt stays untouched so AltGr
+            // (QwertyLafayette on RALT_T) keeps working on both OSes.
+            keymap_config.swap_lalt_lgui = !keymap_config.swap_lalt_lgui;
+            keymap_config.swap_ralt_rgui = false;
+            eeconfig_update_keymap(&keymap_config);
+            return false;
     }
 
     return true;
@@ -202,6 +248,9 @@ static void render_status(void) {
             break;
         }
     }
+
+    oled_write_P(PSTR("OS: "), false);
+    oled_write_P(keymap_config.swap_lalt_lgui ? PSTR("Mac\n") : PSTR("Win\n"), false);
 
     #ifdef RGB_MATRIX_ENABLE
     oled_write_P(PSTR("RGB: "), false);
