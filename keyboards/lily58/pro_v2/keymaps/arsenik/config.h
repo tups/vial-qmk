ceb6fd283c4d687710c9f991d4361c80fd8fc8be
Damien TUPINIER <tupinier.damien@gmail.com>
2026-06-08 10:50:50 +0200

    Adjust tapping term and refactor layers in Arsenik keymap for Lily58 Pro V2

--- keyboards/lily58/pro_v2/keymaps/arsenik/config.h
+++ keyboards/lily58/pro_v2/keymaps/arsenik/config.h
@@ -127,7 +127,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
-#define ARSENIK_HRM_TAPPING_TERM 300
+#define ARSENIK_HRM_TAPPING_TERM 150
 /* This is the delay used by Arsenik for sensitive tap-holds, which include
  * home-row-mods and mod-taps using the spacebar
  */
